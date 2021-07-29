08786ddafe3a88065a7b7c9eb5ce2ee2e2e9fe8b
Ryan McBride <mr.ryan444@gmail.com>
2021-07-28 20:21:56 -0600

    Add key/values to animal object

--- object-examples.js
+++ object-examples.js
@@ -6,3 +6,12 @@ var car = {
     color: 'Beige',
     isRunning: false
 }
+
+var animal = {
+    type: 'dog',
+    name: 'Cat Stevens',
+    age: 2,
+    color: 'Brown',
+    isChewingMyCouch: true,
+    toys: ['big rope', 'my shoe', 'squeeky fish']
+}
